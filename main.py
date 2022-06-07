45fd391bb42383021b2f7c155ede3f4cd3a2fb33
BAK BAK Bakr <aaronbakr89@gmail.com>
2022-06-07 19:51:27 -0400

    added list

---
+++ main.py
@@ -0,0 +1,4 @@
+destinations_list = ["San Juan", "Aruba", "Venice", "Italy"]
+restaurant_list = ["Bak Steakhouse", "Pizzeria", "Steak 48", "Cubre Libre"]
+transportation_list = ["Rental", "Train", "Bike", "Walk"]
+entertainment_list = ["Concert", "Studio", "Museum", "Nap"]
